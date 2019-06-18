f892d0804891d1c8e6c72c02353b6b964ae04635
Victor Henrique Gonçalves <victor@debian>
2019-06-18 17:02:34 -0300

    Tem que ser usado o caminho relativo ao arquivo nasdaqlisted.txt, e não o absoluto. Caso contrário, o programa funcionará somente na máquina na qual ele foi desenvolvido.
    A chave a ser usada é a de quem vai rodar o programa e pode ser obtida
    gratuitamente no site da Alpha Vantage
    
    Intervalo foi aumentado para 5 minutos devido às limitações da chave gratuita
    do Alpha Vantage (5 chamadas por minuto e 500 chamadas por dia)

--- Trabalho 2/api_alpha-vantage.py
+++ Trabalho 2/api_alpha-vantage.py
@@ -13,11 +13,16 @@ from alpha_vantage.timeseries import TimeSeries
 #import matplotlib.pyplot as plt
 import random
 
-file = open("C:\\Users\\Daniel\\Desktop\\Faculdade\\COMPUTAÇÃO\\Inteligência Artificial\\Aprendizado de Máquina\\Trabalhos\\T2\\nasdaqlisted.txt")
+#Tem que ser usado o caminho relativo ao arquivo nasdaqlisted.txt, e não o absoluto. Caso contrário, o programa funcionará somente na máquina na qual ele foi desenvolvido.
+
+#file = open("C:\\Users\\Daniel\\Desktop\\Faculdade\\COMPUTAÇÃO\\Inteligência Artificial\\Aprendizado de Máquina\\Trabalhos\\T2\\nasdaqlisted.txt")
+file = open("nasdaqlisted.txt")
 strFile = file.read() #lê todo o arquivo
 strFile = strFile.splitlines() #lista sem caracter '\n'
 
-ts = TimeSeries(key='614V0UUUNIY1NDMF', output_format='pandas') #Objeto da classe TimeSeries
+#A chave a ser usada é a de quem vai rodar o programa e pode ser obtida gratuitamente no site da Alpha Vantage
+#ts = TimeSeries(key='614V0UUUNIY1NDMF', output_format='pandas') #Objeto da classe TimeSeries
+ts = TimeSeries(key='S55HHK4M4E665SA7', output_format='pandas') #Objeto da classe TimeSeries
 
 symbol_list = [] #Lista contendo símbolos das empresas (ticker symbols)
 data_list = []  #lista contendo dataframes com dados das empresas
@@ -25,8 +30,9 @@ data_list = []  #lista contendo dataframes com dados das empresas
 for i in range (5):
     symbol_list.append(strFile[random.randint(1,3445)])
 
+#Intervalo foi aumentado para 5 minutos devido às limitações da chave gratuita do Alpha Vantage (5 chamadas por minuto e 500 chamadas por dia)
 for s in symbol_list:
-    data_list.append(ts.get_intraday(symbol= s ,interval='1min', outputsize='full'))
+    data_list.append(ts.get_intraday(symbol= s ,interval='5min', outputsize='full'))
 
 i = 0
 for x in data_list:
@@ -34,8 +40,8 @@ for x in data_list:
     data_list[i] = x
     i = i + 1
     
-#data, meta_data = ts.get_intraday(symbol='MSFT',interval='1min', outputsize='full')
+#data, meta_data = ts.get_intraday(symbol='MSFT',interval='5min', outputsize='full')
 
 #data['1. open'].plot()
-#plt.title('Intraday Times Series for the MSFT stock (1 min)')
+#plt.title('Intraday Times Series for the MSFT stock (5 min)')
 #plt.show()
