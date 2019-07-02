b9832ba17bbcb99f7ef78bcb8316115c77ac266c
Victor Henrique Gonçalves <victor@debian>
2019-07-02 11:08:26 -0300

    Adicionado PCA ao Algoritmo

--- Trabalho 2/trab2.py
+++ Trabalho 2/trab2.py
@@ -11,8 +11,11 @@ import numpy as np
 
 from sklearn.cluster import DBSCAN
 from sklearn import metrics
-from sklearn.model_selection import TimeSeriesSplit
+#from sklearn.model_selection import train_test_split
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.model_selection import cross_val_score
 
 import matplotlib.pyplot as plt
 
@@ -35,29 +38,28 @@ for arq in csvs:
     
     csvStock = csvStock.dropna()
     
-
-    csvStock['OpenPct']  = csvStock['open'].pct_change()
-    csvStock['HighPct']  = csvStock['high'].pct_change()
-    csvStock['LowPct']   = csvStock['low'].pct_change()
-    csvStock['ClosePct'] = csvStock['close'].pct_change()
     csvStock['Name']     = arq.replace(".csv","")
     csvStock['Name']     = csvStock['Name'].astype('category')
 
-    csvStock = csvStock[['OpenPct','HighPct','LowPct','ClosePct','Name']].dropna()
+csvStock['OpenPct']  = csvStock['open'].pct_change()
+csvStock['HighPct']  = csvStock['high'].pct_change()
+csvStock['LowPct']   = csvStock['low'].pct_change()
+csvStock['ClosePct'] = csvStock['close'].pct_change()
+
+csvStock = csvStock[['OpenPct','HighPct','LowPct','ClosePct','Name']].dropna()
 
 #os dados obtidos dos arquivos csv são transformados em um vetor NumPy
 
-raw_data = np.array(csvStock.iloc[:,:-1])
+train_data = np.array(csvStock.iloc[:,:-1])
+class_data = np.array(csvStock.iloc[:,-1])
 
 #uma técnica TimeSeriesSplit é usada para dividir a série temporal em treino e teste
 #no momento, usamos apenas os dados de treinamento
 #a utilização dos dados de teste será implementada posteriormente
 
-tscv = TimeSeriesSplit(n_splits=10)
-for train_index, test_index in tscv.split(raw_data):
-      train_data, test_data = raw_data[train_index], raw_data[test_index]
+#X_train, X_test, y_train, y_test = train_test_split(train_data, class_data, test_size=0.2)
 
-labels_true = np.array(train_data[:,-1])
+labels_true = np.array(class_data)
 
 #aqui é executado o algoritmo de agrupamento baseado em densidade DBSCAN
 #o valor eps=0.003 obteve o melhor resultado conforme visualização gráfica
@@ -133,7 +135,171 @@ for name, est in estimators:
     axs[fignum].set_yticklabels([])
     axs[fignum].set_xlabel('Fechamento')
     axs[fignum].set_ylabel('Abertura')
-    axs[fignum].set_title(titles[fignum - 1])
+    axs[fignum].set_title(titles[fignum])
 
     fignum = fignum + 1
 plt.show()
+
+x = StandardScaler().fit_transform(train_data)
+pca = PCA(n_components=2)
+principalComponents = pd.DataFrame(pca.fit_transform(x))
+
+principalComponents.plot.scatter(x=0,y=1)
+plt.show()
+
+df_class = pd.DataFrame(class_data)
+principalComponents_train = pd.concat([principalComponents,df_class],axis=1)
+
+#aqui é executado o algoritmo de agrupamento baseado em densidade DBSCAN
+#o valor eps=0.003 obteve o melhor resultado conforme visualização gráfica
+
+labels_true = np.array(class_data)
+
+db = DBSCAN(eps=0.10, min_samples=10).fit(principalComponents)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+#abaixo estão algumas medidas obtidas do DBSCAN
+
+print('')
+print('Número estimado de grupos: %d' % n_clusters_)
+print('Número estimado de outliers: %d' % n_noise_)
+print("Homogeneidade: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+print("Completude: %0.3f" % metrics.completeness_score(labels_true, labels))
+print("V-medida: %0.3f" % metrics.v_measure_score(labels_true, labels))
+print("Índice Rand Ajustado: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
+print("Informação Mútua Ajustada: %0.3f" % metrics.adjusted_mutual_info_score(labels_true, labels))
+print("Coeficiente Silhueta: %0.3f" % metrics.silhouette_score(principalComponents, labels))
+
+#esta parte gera e mostra o gráfico dos grupos obtidos com o DBSCAN
+#na melhor configuração, foi obtido um único grupo e 4 outliers
+
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        col = [0, 0, 0, 1]
+
+    class_member_mask = (labels == k)
+
+    xy = principalComponents_train[class_member_mask & core_samples_mask]
+    plt.plot(xy.iloc[:, 0], xy.iloc[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=14)
+
+    xy = principalComponents_train[class_member_mask & ~core_samples_mask]
+    plt.plot(xy.iloc[:, 0], xy.iloc[:, 1], 'o', markerfacecolor=tuple(col),
+             markeredgecolor='k', markersize=6)
+
+plt.title('Número estimado de grupos: %d' % n_clusters_)
+plt.show()
+
+#esta parte é uma comparação com o k-Means
+#foi utilizada a configuração com 5 clusters
+
+kmeans = KMeans(n_clusters=5, random_state=0).fit_predict(principalComponents)
+
+plt.scatter(principalComponents_train.iloc[:, 0], principalComponents_train.iloc[:, 1], c=kmeans)
+plt.title("Divisão em grupos estimada")
+plt.show()
+
+#outra comparação com o k-Means
+#nesta comparação foi utilizadas configurações com 3 e 8 clusters
+
+estimators = [('k_means_8', KMeans(n_clusters=8)),
+              ('k_means_3', KMeans(n_clusters=3))]
+
+fignum = 0
+fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(4,6))
+fig.subplots_adjust(hspace=0.7)
+titles = ['8 clusters', '3 clusters']
+for name, est in estimators:
+    est.fit(principalComponents)
+    labels = est.labels_
+
+    axs[fignum].scatter(principalComponents_train.iloc[:, 0], principalComponents_train.iloc[:, 1],
+                  c=labels.astype(np.float), edgecolor='k')
+    axs[fignum].set_xticklabels([])
+    axs[fignum].set_yticklabels([])
+#    axs[fignum].set_xlabel('Fechamento')
+#    axs[fignum].set_ylabel('Abertura')
+    axs[fignum].set_title(titles[fignum])
+
+    fignum = fignum + 1
+plt.show()
+
+'''
+x = StandardScaler().fit_transform(X_test)
+pca = PCA(n_components=2)
+principalComponents = pd.DataFrame(pca.fit_transform(x))
+
+principalComponents.plot.scatter(x=0,y=1)
+plt.show()
+
+df_class = pd.DataFrame(y_test)
+principalComponents_test = pd.concat([principalComponents,df_class],axis=1)
+
+
+#aqui é executado o algoritmo de agrupamento baseado em densidade DBSCAN
+#o valor eps=0.003 obteve o melhor resultado conforme visualização gráfica
+
+labels_true = np.array(y_test)
+
+db = DBSCAN(eps=0.10, min_samples=10).fit(principalComponents)
+core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
+core_samples_mask[db.core_sample_indices_] = True
+labels = db.labels_
+
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+n_noise_ = list(labels).count(-1)
+
+#abaixo estão algumas medidas obtidas do DBSCAN
+
+print('')
+print('Número estimado de grupos: %d' % n_clusters_)
+print('Número estimado de outliers: %d' % n_noise_)
+print("Homogeneidade: %0.3f" % metrics.homogeneity_score(labels_true, labels))
+print("Completude: %0.3f" % metrics.completeness_score(labels_true, labels))
+print("V-medida: %0.3f" % metrics.v_measure_score(labels_true, labels))
+print("Índice Rand Ajustado: %0.3f" % metrics.adjusted_rand_score(labels_true, labels))
+print("Informação Mútua Ajustada: %0.3f" % metrics.adjusted_mutual_info_score(labels_true, labels))
+#print("Coeficiente Silhueta: %0.3f" % metrics.silhouette_score(principalComponents, labels))
+
+#esta parte é uma comparação com o k-Means
+#foi utilizada a configuração com 5 clusters
+
+kmeans = KMeans(n_clusters=5, random_state=0).fit_predict(principalComponents)
+
+plt.scatter(principalComponents_test.iloc[:, 0], principalComponents_test.iloc[:, 1], c=kmeans)
+plt.title("Divisão em grupos estimada")
+plt.show()
+
+#outra comparação com o k-Means
+#nesta comparação foi utilizadas configurações com 3 e 8 clusters
+
+estimators = [('k_means_8', KMeans(n_clusters=8)),
+              ('k_means_3', KMeans(n_clusters=3))]
+
+fignum = 0
+fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(4,6))
+fig.subplots_adjust(hspace=0.7)
+titles = ['8 clusters', '3 clusters']
+for name, est in estimators:
+    est.fit(principalComponents)
+    labels = est.labels_
+
+    axs[fignum].scatter(principalComponents_test.iloc[:, 0], principalComponents_test.iloc[:, 1],
+                  c=labels.astype(np.float), edgecolor='k')
+    axs[fignum].set_xticklabels([])
+    axs[fignum].set_yticklabels([])
+#    axs[fignum].set_xlabel('Fechamento')
+#    axs[fignum].set_ylabel('Abertura')
+    axs[fignum].set_title(titles[fignum])
+
+    fignum = fignum + 1
+plt.show()
+'''
